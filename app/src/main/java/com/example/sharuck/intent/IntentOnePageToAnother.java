49e2d0c648ddf80b8f158c9730d61d6c31032516
SHARUCK <sharuck.sid@gmail.com>
2018-10-17 16:01:59 +0600

    My first comit

---
+++ app/src/main/java/com/example/sharuck/intent/IntentOnePageToAnother.java
@@ -0,0 +1,13 @@
+package com.example.sharuck.intent;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class IntentOnePageToAnother extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intent_one_page_to_another);
+    }
+}
